a3ee68a05a398ef47188d75ca70fd83e26dcb1ca
Dominik Nakamura <dnaka91@gmail.com>
2022-07-09 22:52:19 +0900

    Implement fetching for git repo crates

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use std::{
     fmt,
     fs::File,
     hash::{Hash, Hasher},
-    path::PathBuf,
 };
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use cargo::{InstallInfo, PackageId};
 use clap::{Args, IntoApp, Parser, Subcommand};
 use clap_complete::Shell;
 use crates_index::Index;
+use git2::{Commit, Oid, Repository};
 use owo_colors::OwoColorize;
 use semver::Version;
 use siphasher::sip::SipHasher24;
 
 use crate::{
-    cargo::{CrateListingV2, SourceKind},
+    cargo::{CrateListingV2, GitReference, SourceKind},
     table::Table,
 };
 
@@ -35,6 +35,8 @@ enum Opt {
 struct Command {
     #[clap(flatten)]
     select_args: SelectArgs,
+    #[clap(short = 'n', long)]
+    dry_run: bool,
     #[clap(subcommand)]
     subcmd: Option<Subcmd>,
 }
@@ -92,9 +94,11 @@ fn main() -> Result<()> {
 
     println!();
 
-    install_registry_updates(updates.registry.into_iter());
-    install_git_updates(updates.git.into_iter());
-    install_path_updates(updates.path.into_iter());
+    if !cmd.dry_run {
+        install_registry_updates(updates.registry.into_iter());
+        install_git_updates(updates.git.into_iter());
+        install_path_updates(updates.path.into_iter());
+    }
 
     Ok(())
 }
@@ -106,7 +110,7 @@ fn load_crate_state() -> Result<CrateListingV2> {
         "crate state".green().bold()
     ));
 
-    let home = cargo_home()?;
+    let home = home::cargo_home()?;
     let path = home.join(".crates2.json");
 
     let file = File::open(path)?;
@@ -138,7 +142,7 @@ fn collect_updates(info: CrateListingV2, index: &Index, args: &SelectArgs) -> Re
 
     for (package, info) in info.installs.into_iter() {
         match package.source_id.kind {
-            SourceKind::Git(_) => {
+            SourceKind::Git(ref git_ref) => {
                 if !args.git {
                     continue;
                 }
@@ -158,14 +162,69 @@ fn collect_updates(info: CrateListingV2, index: &Index, args: &SelectArgs) -> Re
                 let hash = hex::encode(hasher.finish().to_le_bytes());
                 let path = format!("{ident}-{hash}");
 
-                updates.git.insert(package, path);
+                let cargo_home = home::cargo_home()?;
+                let repo_path = cargo_home.join("git/db").join(&path);
+
+                if repo_path.is_dir() {
+                    let repo = Repository::open_bare(repo_path)?;
+                    let mut remote = repo.remote_anonymous(package.source_id.url.as_str())?;
+
+                    let target = match git_ref {
+                        GitReference::Tag(_) => todo!("git tag"),
+                        GitReference::Branch(b) => b,
+                        GitReference::Rev(_) => continue, // don't move pinned revs
+                        GitReference::DefaultBranch => "HEAD",
+                    };
+
+                    remote.fetch(&[target], None, None)?;
+
+                    let head = match repo.find_reference(&format!("refs/remotes/origin/{target}")) {
+                        Ok(r) => r.peel_to_commit()?,
+                        Err(e) => bail!(
+                            "HEAD error class: {:?}, code: {:?}, msg: {}",
+                            e.class(),
+                            e.code(),
+                            e.message()
+                        ),
+                    };
+                    let fetch_head = match repo.find_reference("FETCH_HEAD") {
+                        Ok(r) => r.peel_to_commit()?,
+                        Err(e) => bail!(
+                            "FETCH_HEAD error class: {:?}, code: {:?}, msg: {}",
+                            e.class(),
+                            e.code(),
+                            e.message()
+                        ),
+                    };
+
+                    let changes = git_changes(&repo, &head, &fetch_head)?;
+
+                    if changes.commits > 0 {
+                        updates.git.insert(
+                            package,
+                            UpdateInfo::new(
+                                info,
+                                GitInfo {
+                                    path,
+                                    old_commit: head.id(),
+                                    new_commit: fetch_head.id(),
+                                    changes,
+                                },
+                            ),
+                        );
+                    }
+                } else {
+                    println!("    missing {}", path.red());
+                }
             }
             SourceKind::Path => {
                 if !args.path {
                     continue;
                 }
 
-                updates.path.insert(package, ());
+                updates
+                    .path
+                    .insert(package, UpdateInfo::new(info, PathInfo {}));
             }
             SourceKind::Registry => {
                 let krate = index
@@ -181,7 +240,10 @@ fn collect_updates(info: CrateListingV2, index: &Index, args: &SelectArgs) -> Re
                 if latest > package.version {
                     if latest.major != package.version.major {}
 
-                    updates.registry.insert(package, (latest, info));
+                    updates.registry.insert(
+                        package,
+                        UpdateInfo::new(info, RegistryInfo { version: latest }),
+                    );
                 }
             }
         }
@@ -192,18 +254,45 @@ fn collect_updates(info: CrateListingV2, index: &Index, args: &SelectArgs) -> Re
 
 #[derive(Default)]
 struct Updates {
-    registry: BTreeMap<PackageId, (Version, InstallInfo)>,
-    git: BTreeMap<PackageId, String>,
-    path: BTreeMap<PackageId, ()>,
+    registry: BTreeMap<PackageId, UpdateInfo<RegistryInfo>>,
+    git: BTreeMap<PackageId, UpdateInfo<GitInfo>>,
+    path: BTreeMap<PackageId, UpdateInfo<PathInfo>>,
 }
 
-fn print_registry_updates(updates: &BTreeMap<PackageId, (Version, InstallInfo)>) {
+struct UpdateInfo<T> {
+    install_info: InstallInfo,
+    extra: T,
+}
+
+impl<T> UpdateInfo<T> {
+    fn new(install_info: InstallInfo, extra: T) -> Self {
+        Self {
+            install_info,
+            extra,
+        }
+    }
+}
+
+struct RegistryInfo {
+    version: Version,
+}
+
+struct GitInfo {
+    path: String,
+    old_commit: Oid,
+    new_commit: Oid,
+    changes: GitChanges,
+}
+
+struct PathInfo {}
+
+fn print_registry_updates(updates: &BTreeMap<PackageId, UpdateInfo<RegistryInfo>>) {
     if updates.is_empty() {
         println!("no {} crate updates", "registry".green());
     } else {
         let registry = updates
             .iter()
-            .map(|(pkg, (latest, _))| (pkg.name.as_str(), &pkg.version, latest))
+            .map(|(pkg, info)| (pkg.name.as_str(), &pkg.version, &info.extra.version))
             .collect::<Table>();
 
         println!("<<< Updates from the {} >>>", "registry".green());
@@ -211,7 +300,7 @@ fn print_registry_updates(updates: &BTreeMap<PackageId, (Version, InstallInfo)>)
     }
 }
 
-fn print_git_updates(updates: &BTreeMap<PackageId, String>, enabled: bool) {
+fn print_git_updates(updates: &BTreeMap<PackageId, UpdateInfo<GitInfo>>, enabled: bool) {
     if !enabled {
         println!(
             "{} crate updates {}",
@@ -223,7 +312,7 @@ fn print_git_updates(updates: &BTreeMap<PackageId, String>, enabled: bool) {
     } else {
         let gits = updates
             .iter()
-            .map(|(pkg, path)| (pkg.name.as_str(), path.as_str()))
+            .map(|(pkg, info)| (pkg.name.as_str(), &info.extra))
             .collect::<BTreeMap<_, _>>();
 
         let width = gits
@@ -234,18 +323,26 @@ fn print_git_updates(updates: &BTreeMap<PackageId, String>, enabled: bool) {
 
         println!("<<< Updates from {} >>>", "git".green());
 
-        println!("\n{:width$}  Git Paths", "Name");
-        println!("{}", "─".repeat(width * 2 + 2 + 1 + 16));
+        println!("\n{:width$}  Old         New      Changes", "Name");
+        println!("{}", "─".repeat(width * 2 + 2 + 1 + 16+20));
 
-        for (name, git) in gits {
-            println!("{name:width$}  {git}");
+        for (name, info) in gits {
+            println!(
+                "{name:width$}  {:.7}  ➞  {:.7}  {:2} commits | {:2} files changed | {} {}",
+                info.old_commit.cyan(),
+                info.new_commit.cyan(),
+                info.changes.commits.yellow(),
+                info.changes.files_changed.white(),
+                format_args!("+{}", info.changes.insertions).green(),
+                format_args!("-{}", info.changes.deletions).red()
+            );
         }
 
         println!("\n");
     }
 }
 
-fn print_path_updates(updates: &BTreeMap<PackageId, ()>, enabled: bool) {
+fn print_path_updates(updates: &BTreeMap<PackageId, UpdateInfo<PathInfo>>, enabled: bool) {
     if !enabled {
         println!(
             "{} crate updates {}",
@@ -259,7 +356,7 @@ fn print_path_updates(updates: &BTreeMap<PackageId, ()>, enabled: bool) {
 
         let paths = updates
             .iter()
-            .map(|(pkg, ())| pkg.name.as_str())
+            .map(|(pkg, _)| pkg.name.as_str())
             .collect::<BTreeSet<_>>();
 
         let width = paths
@@ -277,17 +374,8 @@ fn print_path_updates(updates: &BTreeMap<PackageId, ()>, enabled: bool) {
     }
 }
 
-fn cargo_home() -> Result<PathBuf> {
-    match std::env::var_os("CARGO_HOME") {
-        Some(home) => Ok(home.into()),
-        None => Ok(dirs::home_dir()
-            .context("failed finding home directory")?
-            .join(".cargo")),
-    }
-}
-
 fn install_registry_updates(
-    updates: impl ExactSizeIterator<Item = (PackageId, (Version, InstallInfo))>,
+    updates: impl ExactSizeIterator<Item = (PackageId, UpdateInfo<RegistryInfo>)>,
 ) {
     let count = updates.len();
 
@@ -298,16 +386,18 @@ fn install_registry_updates(
             "registry".green().bold()
         );
 
-        for (i, (pkg, (latest, info))) in updates.enumerate() {
+        for (i, (pkg, info)) in updates.enumerate() {
             println!(
                 "\n{} updating {} from {} to {}",
                 format_args!("[{}/{}]", i + 1, count).bold(),
                 pkg.name.green().bold(),
                 pkg.version.blue().bold(),
-                latest.blue().bold()
+                info.extra.version.blue().bold()
             );
 
-            if let Err(e) = cargo_install_from_registry(&pkg.name, &latest, &info) {
+            if let Err(e) =
+                cargo_install_from_registry(&pkg.name, &info.extra.version, &info.install_info)
+            {
                 println!(
                     "installing {} {}:\n{e}",
                     pkg.name.green().bold(),
@@ -318,7 +408,7 @@ fn install_registry_updates(
     }
 }
 
-fn install_git_updates(updates: impl ExactSizeIterator<Item = (PackageId, String)>) {
+fn install_git_updates(updates: impl ExactSizeIterator<Item = (PackageId, UpdateInfo<GitInfo>)>) {
     let count = updates.len();
 
     if count > 0 {
@@ -330,7 +420,7 @@ fn install_git_updates(updates: impl ExactSizeIterator<Item = (PackageId, String
     }
 }
 
-fn install_path_updates(updates: impl ExactSizeIterator<Item = (PackageId, ())>) {
+fn install_path_updates(updates: impl ExactSizeIterator<Item = (PackageId, UpdateInfo<PathInfo>)>) {
     let count = updates.len();
 
     if count > 0 {
@@ -394,6 +484,31 @@ fn progress(msg: fmt::Arguments) -> ProgressGuard {
     ProgressGuard
 }
 
+struct GitChanges {
+    commits: usize,
+    files_changed: usize,
+    insertions: usize,
+    deletions: usize,
+}
+
+fn git_changes<'r>(repo: &'r Repository, old: &Commit<'r>, new: &Commit<'r>) -> Result<GitChanges> {
+    let (ahead, behind) = repo.graph_ahead_behind(old.id(), new.id())?;
+    if ahead != 0 {
+        println!("hm... local HEAD shouldn't be ahead of remote HEAD");
+    }
+
+    let diff_stats = repo
+        .diff_tree_to_tree(Some(&old.tree()?), Some(&new.tree()?), None)?
+        .stats()?;
+
+    Ok(GitChanges {
+        commits: behind,
+        files_changed: diff_stats.files_changed(),
+        insertions: diff_stats.insertions(),
+        deletions: diff_stats.deletions(),
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -402,5 +517,53 @@ mod tests {
     fn verify_app() {
         use clap::CommandFactory;
         Opt::command().debug_assert();
+    }
+
+    #[test]
+    fn parse_thingy() -> anyhow::Result<()> {
+        let cargo_home = home::cargo_home()?;
+        let repo = cargo_home.join("git/db/oxker-0e3309108aa09323");
+        let repo = Repository::open_bare(repo)?;
+
+        // Fetch the latest HEAD from remote.
+        let mut remote = repo.remote_anonymous("https://github.com/mrjackwills/oxker.git")?;
+        remote.fetch(&["HEAD"], None, None)?;
+
+        // Get current HEAD and just fetched new FETCH_HEAD.
+        let ref_old = repo
+            .find_reference("refs/remotes/origin/HEAD")?
+            .peel_to_commit()?;
+        let ref_new = repo.find_reference("FETCH_HEAD")?.peel_to_commit()?;
+
+        // Compare the two, counting amount of commits.
+        let GitChanges {
+            commits,
+            files_changed,
+            insertions,
+            deletions,
+        } = git_changes(&repo, &ref_old, &ref_new)?;
+
+        println!(
+            "{:.7} -> {:.7} ({} commits | {} files changed | {} {})",
+            ref_old.id().cyan(),
+            ref_new.id().cyan(),
+            commits.yellow().bold(),
+            files_changed.white(),
+            format_args!("+{insertions}").green(),
+            format_args!("-{deletions}").red(),
+        );
+
+        // Update HEAD to FETCH_HEAD.
+        // r_old.set_target(r_new.id(), "")?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn int_len() {
+        let value = 150usize;
+        let len = (value as f64).log10().floor() as usize + 1;
+
+        assert_eq!(3, len);
     }
 }
